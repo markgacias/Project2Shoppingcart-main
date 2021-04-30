9047f53280e53c08168f63afede2dbd4512ac9b9
markgacias <markgacias@gmail.com>
2021-04-30 16:58:49 +0800

    Added Courses into the Shopping Cart

--- StarterFileshoppingcart-main/js/app.js
+++ StarterFileshoppingcart-main/js/app.js
@@ -1,5 +1,6 @@
 // Variables
-const courses = document.querySelector('#courses-list');
+const courses = document.querySelector('#courses-list'),
+shoppingCartContent = document.querySelector('#cart-content tbody');
 
 
 
@@ -35,5 +36,39 @@ function buyCourse(e)
 
 function getCourseInfo(course)
 {
-    console.log(course);
+    //Create an Object with Course Data
+    const courseInfo = 
+    {
+        image: course.querySelector('img').src,
+        title: course.querySelector('h4').textContent,
+        price: course.querySelector('.price span').textContent,
+        id: course.querySelector('a').getAttribute('data-id')
+
+    }
+    //Insert into the shopping cart
+    addIntoCart(courseInfo);
+}
+//Display the selected course into the shopping cart
+
+function addIntoCart(course)
+{
+    //create a <tr>
+    const row = document.createElement('tr');
+
+    //Build the template
+    row.innerHTML = `
+        <tr>
+            <td>
+                <img src="${course.image}" width=100>
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                <a href="#" class="remove" data-id="${course.id}">X</a>
+            </td>
+
+        </tr>
+    `;
+    //Add into the shopping cart
+    shoppingCartContent.appendChild(row);
 }
